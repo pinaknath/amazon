001b0a398d905ab8fe56e820ad366eea748bddbc
pinaknath <pinak.nath1@outlook.com>
2019-02-04 21:40:42 +0530

    :x

---
+++ file.java
@@ -0,0 +1,13 @@
+public class
+{
+	public static void main(string args[])
+	{
+		if(i=2, n<=0, i++)
+		{
+			n/2==0;
+			syste.out.println("Number is prime");
+			else{
+				system.out.println("NUmber is not prime")
+			}
+		}
+	}
